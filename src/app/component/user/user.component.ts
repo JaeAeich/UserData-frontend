d722c93b6f0aa162e1a0088de87b814914375c77
JaeAeich <javedh@iitbhilai.ac.in>
2023-02-17 11:21:48 +0530

    edit feature completed

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -1,50 +1,3 @@
-// import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-
-// @Component({
-//   selector: 'app-user',
-//   templateUrl: './user.component.html',
-//   styleUrls: ['./user.component.css'],
-// })
-// export class UserComponent implements OnInit {
-//   users: any = [];
-//   isEditting = false;
-//   editedUser: any = {};
-
-//   @ViewChild('modalTriggerButton') modalTriggerButton!: ElementRef; // Added
-
-//   constructor(private http: HttpClient) {}
-
-//   ngOnInit() {
-//     this.getUsers();
-//   }
-
-//   getUsers() {
-//     this.http.get('http://localhost:4000/person').subscribe((data: any) => {
-//       this.users = data;
-//     });
-//   }
-
-//   editUser(id: string) {
-//     this.isEditting = true;
-//     console.log(`Editing user with ID: ${id}`);
-//     this.editedUser = this.users.find((user: any) => user._id === id);
-//     this.modalTriggerButton.nativeElement.click(); // Added
-//   }
-//   submitEdit() {
-//     console.log(this.editedUser);
-//     this.modalTriggerButton.nativeElement.click();
-//   }
-
-//   deleteUser(id: string) {
-//     if (confirm('Are you sure you want to delete this user?')) {
-//       this.http.delete(`http://localhost:4000/person/${id}`).subscribe(() => {
-//         this.getUsers();
-//       });
-//     }
-//   }
-// }
-
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -59,7 +12,7 @@ export class UserComponent implements OnInit {
   editedUser: any = {};
   newUserData: any = {};
 
-  @ViewChild('modalTriggerButton') modalTriggerButton!: ElementRef; // Added
+  @ViewChild('modalTriggerButton') modalTriggerButton!: ElementRef;
 
   constructor(private http: HttpClient) {}
 
@@ -75,17 +28,33 @@ export class UserComponent implements OnInit {
 
   editUser(id: string) {
     this.isEditting = true;
-    console.log(`Editing user with ID: ${id}`);
+    // console.log(`Editing user with ID: ${id}`);
     this.editedUser = this.users.find((user: any) => user._id === id);
-    this.newUserData = { ...this.editedUser }; // Added
+    this.newUserData = { ...this.editedUser };
     this.modalTriggerButton.nativeElement.click();
   }
 
   submitEdit() {
-    console.log(this.editedUser); // Updated
-    this.editedUser = { ...this.newUserData }; // Updated
+    // console.log(this.editedUser);
+    // send a PUT request to the backend to update the user data
+    const url = `http://localhost:4000/person/${this.editedUser._id}`;
+    const body = {
+      name: this.editedUser.name,
+      age: this.editedUser.age,
+      gender: this.editedUser.gender,
+      mobile: this.editedUser.mobile,
+    };
+    this.http.put(url, body).subscribe(
+      (response) => {
+        console.log('Update successful', response);
+      },
+      (error) => {
+        console.error('Update failed', error);
+      }
+    );
+    this.editedUser = { ...this.newUserData }; 
     this.modalTriggerButton.nativeElement.click();
-    this.newUserData = {}; // Added
+    this.newUserData = {}; 
   }
 
   deleteUser(id: string) {
